31569d861319880105cbcd22766023347fd8f76b
Yuguantao <Guantaoyu@yeah.net>
2017-06-23 19:30:06 +0800

    add 20170623

---
+++ 0623/20170623/shop/scripts/common.js
@@ -0,0 +1,34 @@
+var navUl = document.getElementById('nav-ul');
+var goodsUl = document.getElementById('goods-ul');
+//获取分类信息
+$.ajax({
+  "url": 'http://lc.shudong.wang/api_cat.php',
+  "type": "GET",
+  "dataType": "json",
+  "success": function(response){
+    var oNavLiTemplateStr = document.getElementById('nav-li-template').innerHTML;
+    //处理返回的数据
+    console.log(response);
+    for (var i = 0; i < response.data.length; i++) {
+      var obj = response.data[i];
+      // if (i === 4 || i === 9) {
+      if (i % 3 === 2) {
+        obj.className = 'red';
+      }
+      obj.cat_name = '(' + obj.cat_name + ')';
+      navUl.innerHTML += $.compile(oNavLiTemplateStr, obj);
+      /*
+      var obj = response.data[i];
+      var oLi = document.createElement('li');
+      var oA = document.createElement('a');
+      oA.innerText = obj.cat_name;
+      oA.href = "list.html?cat_id=" + obj.cat_id;
+      oLi.appendChild(oA);
+      navUl.appendChild(oLi);
+      */
+    }
+  },
+  "error": function(message) {
+    console.log(message);
+  }
+});
